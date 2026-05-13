82cd12169f82d9543f036610c7aea12a6fdeb606
seoyeon <hemsyeons@gmail.com>
2026-05-14 02:38:26 +0900

    chore: 프로젝트 초기 세팅

---
+++ 38-SOPKATHON-iOS-iOS1/38-SOPKATHON-iOS-iOS1/Features/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  38-SOPKATHON-iOS-iOS1
+//
+//  Created by 신서연 on 5/14/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
+
