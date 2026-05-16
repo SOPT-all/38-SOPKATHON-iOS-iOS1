54fe31a0f3c864e46687fba8785d653ae6e6a05b
seoyeon <hemsyeons@gmail.com>
2026-05-17 00:19:59 +0900

    design: 완료한 목표 섹션 추가

--- 38-SOPKATHON-iOS-iOS1/38-SOPKATHON-iOS-iOS1/Global/Components/MemorySpaceCell.swift
+++ 38-SOPKATHON-iOS-iOS1/38-SOPKATHON-iOS-iOS1/Global/Components/MemorySpaceCell.swift
@@ -43,12 +43,11 @@ final class MemorySpaceCell: UITableViewCell {
     }
 
     private let enterButton = UIButton(type: .system).then {
-        $0.setTitle("입장", for: .normal)
+        $0.setTitle("초대", for: .normal)
         $0.setTitleColor(.white, for: .normal)
-        $0.titleLabel?.font = .systemFont(ofSize: 15, weight: .semibold)
-        $0.backgroundColor = .gray500
-        $0.layer.cornerRadius = 10
-        $0.layer.borderWidth = 1
+        $0.titleLabel?.font = .systemFont(ofSize: 14, weight: .semibold)
+        $0.backgroundColor = .systemGray5
+        $0.layer.cornerRadius = 6
         $0.clipsToBounds = true
     }
 
@@ -92,8 +91,8 @@ final class MemorySpaceCell: UITableViewCell {
 private extension MemorySpaceCell {
 
     func setStyle() {
-        backgroundColor = .black
-        contentView.backgroundColor = .black
+        backgroundColor = .white
+        contentView.backgroundColor = .white
         selectionStyle = .none
     }
 
@@ -122,8 +121,8 @@ private extension MemorySpaceCell {
         enterButton.snp.makeConstraints {
             $0.trailing.equalToSuperview()
             $0.centerY.equalToSuperview()
-            $0.width.equalTo(112)
-            $0.height.equalTo(72)
+            $0.width.equalTo(55)
+            $0.height.equalTo(35)
         }
     }
 }
