f5916927361833e96fcc175720107bb4bd4bd360
seoyeon <hemsyeons@gmail.com>
2026-05-16 19:24:13 +0900

    design: UIColor+ 추가

---
+++ 38-SOPKATHON-iOS-iOS1/38-SOPKATHON-iOS-iOS1/Global/Extensions/UIColor+.swift
@@ -0,0 +1,29 @@
+//
+//  UIColor+.swift
+//  38-COLLABORATION-iOS-DAANGN
+//
+//  Created by 신서연 on 5/16/26.
+//
+
+import UIKit
+
+extension UIColor {
+
+    // MARK: - Base
+
+    static let black = UIColor(named: "black")
+    static let white = UIColor(named: "white")
+
+    // MARK: - Cool Neutral
+
+    static let gray50 = UIColor(named: "gray50")
+    static let gray100 = UIColor(named: "gray100")
+    static let gray200 = UIColor(named: "gray200")
+    static let gray300 = UIColor(named: "gray300")
+    static let gray400 = UIColor(named: "gray400")
+    static let gray500 = UIColor(named: "gray500")
+    static let gray600 = UIColor(named: "gray600")
+    static let gray700 = UIColor(named: "gray700")
+    static let gray800 = UIColor(named: "gray800")
+    static let gray900 = UIColor(named: "gray900")
+}
