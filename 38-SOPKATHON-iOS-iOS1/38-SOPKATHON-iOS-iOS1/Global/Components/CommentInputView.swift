60500dde3585bf7c14cb696b3c217b663911dcce
SeongHwan <epaktldk0203@gmail.com>
2026-05-17 03:57:58 +0900

    design: 댓글 입력 컴포넌트 디자인 및 레이아웃 수정

--- 38-SOPKATHON-iOS-iOS1/38-SOPKATHON-iOS-iOS1/Global/Components/CommentInputView.swift
+++ 38-SOPKATHON-iOS-iOS1/38-SOPKATHON-iOS-iOS1/Global/Components/CommentInputView.swift
@@ -12,7 +12,7 @@ import Then
 
 final class CommentInputView: UIView {
 
-    private let textField = MirumTextField()
+    private let textField = UITextField()
     private let sendButton = UIButton()
 
     override init(frame: CGRect) {
@@ -28,23 +28,33 @@ final class CommentInputView: UIView {
 
     private func setStyle() {
         self.do {
-            $0.backgroundColor = UIColor(red: 239/255, green: 239/255, blue: 239/255, alpha: 1)
-            $0.layer.cornerRadius = 12
+            $0.backgroundColor = .yellow
+            $0.layer.cornerRadius = 32
             $0.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            $0.clipsToBounds = true
         }
 
         textField.do {
+            $0.font = UIFont.body_m_14.font
+            $0.textColor = .black
             $0.backgroundColor = .white
-            $0.font = UIFont.body_m_16.font
-            $0.placeholder = "댓글을 입력해 주세요"
-            $0.setLeftPadding(12)
+            $0.layer.borderColor = UIColor.gray300?.cgColor
+            $0.layer.borderWidth = 1
+            $0.layer.cornerRadius = 8
+            $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: 0))
+            $0.leftViewMode = .always
+            $0.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: 0))
+            $0.rightViewMode = .always
+            $0.attributedPlaceholder = NSAttributedString(
+                string: "마지막 인사를 남겨주세요.",
+                attributes: [.foregroundColor: UIColor.gray400]
+            )
         }
 
         sendButton.do {
-            $0.backgroundColor = UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1)
-            $0.layer.cornerRadius = 22
+            $0.backgroundColor = .clear
             $0.setImage(UIImage(resource: .send), for: .normal)
-            $0.tintColor = .white
+            $0.tintColor = .gray600
         }
     }
 
@@ -54,21 +64,20 @@ final class CommentInputView: UIView {
 
     private func setLayout() {
         textField.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(16)
-            $0.centerY.equalToSuperview()
-            $0.height.equalTo(44)
-            $0.width.equalTo(280)
+            $0.leading.equalToSuperview().inset(27.5)
+            $0.top.equalToSuperview().inset(24)
+            $0.bottom.equalToSuperview().inset(37)
         }
 
         sendButton.snp.makeConstraints {
-            $0.leading.equalTo(textField.snp.trailing).offset(8)
-            $0.trailing.equalToSuperview().inset(16)
-            $0.centerY.equalToSuperview()
-            $0.size.equalTo(44)
+            $0.leading.equalTo(textField.snp.trailing).offset(16)
+            $0.trailing.equalToSuperview().inset(27.5)
+            $0.centerY.equalTo(textField)
+            $0.size.equalTo(24)
         }
 
         self.snp.makeConstraints {
-            $0.height.equalTo(76)
+            $0.height.equalTo(108)
         }
     }
 }
